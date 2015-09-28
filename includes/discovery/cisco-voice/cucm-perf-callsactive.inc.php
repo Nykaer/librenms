da47e43dcbe45696cfb8d1a4b95acdc9746481fc
Aaron Daniels <aaron@daniels.id.au>
2015-09-28 16:46:44 +1000

    - Scrapped existing http function with one sourced from the web with better error handling. - Began discovery module.

---
+++ includes/discovery/cisco-voice/cucm-perf-callsactive.inc.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * LibreNMS module to Graph ActiveCalls on a Cisco CallManager Server
+ *
+ * Copyright (c) 2015 Aaron Daniels <aaron@daniels.id.au>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ */
+
