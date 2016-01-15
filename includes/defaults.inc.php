069af2e055ae85befbf55ef2e6771c32d1ee87bf
Aaron Daniels <aaron@daniels.id.au>
2016-01-15 16:33:05 +1000

    - Moved HTML from Applications to Routing

--- includes/defaults.inc.php
+++ includes/defaults.inc.php
@@ -709,6 +709,7 @@ $config['poller_modules']['cisco-asa-firewall']          = 1;
 $config['poller_modules']['mib'] = 0;
 $config['poller_modules']['cisco-voice']                 = 1;
 $config['poller_modules']['stp']                         = 1;
+$config['poller_modules']['cisco-otv']                   = 1;
 
 // List of discovery modules. Need to be in this array to be
 // considered for execution.
