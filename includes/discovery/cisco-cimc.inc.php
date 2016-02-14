65ccec9fdf32cb3f5189e80f897834d2b42245c3
Aaron Daniels <aaron@daniels.id.au>
2016-02-15 06:25:18 +1000

    - CIMC discovery module

--- includes/discovery/cisco-cimc.inc.php
+++ includes/discovery/cisco-cimc.inc.php
@@ -11,7 +11,7 @@
  * the source code distribution for details.
  */
 
-if ($device['os_group'] == 'cisco') {
+if ($device['os'] == 'cimc') {
 
     $module = 'Cisco-CIMC';
     echo $module.': ';
@@ -27,22 +27,22 @@ if ($device['os_group'] == 'cisco') {
     $tblCIMC = array();
 
     // Let's gather some data..
-    $tblChassis = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.9.35', 2);
-    $tblBoard = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.9.6', 2);
-    $tblFans = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.15.12');
-    $tblPSU = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.15.56');
-    $tblMemory = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.30.11');
-    $tblCPU = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.41.9');
-    $tblStorage = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.1');
-    $tblDisk = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.4');
-    $tblLUN = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.8');
-    $tblBAT = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.11');
+    $tblUCSObjects = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1', 2);
+//    $tblBoard = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.9.6', 2);
+//    $tblFans = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.15.12');
+//    $tblPSU = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.15.56');
+//    $tblMemory = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.30.11');
+//    $tblCPU = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.41.9');
+//    $tblStorage = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.1');
+//    $tblDisk = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.4');
+//    $tblLUN = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.8');
+//    $tblBAT = snmpwalk_array_num($device, '.1.3.6.1.4.1.9.9.719.1.45.11');
 
     /*
      * False == no object found - this is not an error, there is no QOS configured
      * null  == timeout or something else that caused an error, the OID's may be present but we couldn't get them.
      */
-    if ( is_null($tblChassis) || is_null($tblBoard) || is_null($tblFans) || is_null($tblPSU) || is_null($tblMemory) || is_null($tblCPU) || is_null($tblStorage) || is_null($tblDisk) || is_null($tblLUN) || is_null($tblBAT) ) {
+    if ( is_null($tblUCSObjects) ) {
         // We have to error here or we will end up deleting all our components.
         echo "Error\n";
     }
@@ -50,81 +50,143 @@ if ($device['os_group'] == 'cisco') {
         // No Error, lets process things.
         d_echo("CIMC Hardware Found:\n");
 
-        foreach ($tblcbQosObjects['1.3.6.1.4.1.9.9.166.1.5.1.1.2'] as $spid => $array) {
+        // First, let's extract any faults, we will use them later.
+        $faults = array();
+        d_echo("\nFaults: ".print_r($tblUCSObjects['1.3.6.1.4.1.9.9.719.1.1.1.1'],TRUE)."\n");
+        foreach ($tblUCSObjects['1.3.6.1.4.1.9.9.719.1.1.1.1'][5] as $fid => $fobj) {
+            $fobj = preg_replace('/\/sys|sys/','',$fobj);
+            $faults[$fobj] = $tblUCSObjects['1.3.6.1.4.1.9.9.719.1.1.1.1'][11][$fid];
+        }
+        unset ($tblUCSObjects['1.3.6.1.4.1.9.9.719.1.1.1.1']);
+        d_echo("\nFault Array: ".print_r($faults,TRUE)."\n");
 
-            foreach ($array as $spobj => $index) {
-                $result = array();
+       foreach ($tblUCSObjects as $tbl => $array) {
 
-                // Produce a unique reproducible index for this entry.
-                $result['UID'] = hash('crc32', $spid."-".$spobj);
+           switch ($tbl) {
+               // Chassis - /sys/rack-unit-1
+               case "1.3.6.1.4.1.9.9.719.1.9.35.1":
+                   $result = array();
+                   foreach ($array[3] as $key => $item) {
+                       $parent = "";      // Chassis has no parent
+                       $result['parent'] = $parent;
+                       $result['label'] = $parent . "/" . $array[3][$key];
+                       $result['serial'] = $array[47][$key];
+                       $result['string'] = $array[32][$key] ." - ". ($array[49][$key]/1024) ."G Mem, ". $array[36][$key] ." CPU, ". $array[35][$key] ." core";
 
-                // Now that we have a valid identifiers, lets add some more data
-                $result['sp-id'] = $spid;
-                $result['sp-obj'] = $spobj;
+                       // Does this entity have an entry in the faults table.
+                       if (isset ($faults[$result['label']])) {
+                           // Yes, report an error
+                           $result['status'] = 0;
+                           $result['error'] = $faults[$result['label']];
+                       }
+                       else {
+                           // No, unset any errors that may exist.
+                           $result['status'] = 1;
+                           $result['error'] = '';
+                       }
 
-                // Add the Type, Policy-map, Class-map, etc.
-                $type = $tblcbQosObjects['1.3.6.1.4.1.9.9.166.1.5.1.1.3'][$spid][$spobj];
-                $result['qos-type'] = $type;
+                       // Add the result to the array.
+                       d_echo("Chassis (".$tbl."): ".print_r($result, true)."\n");
+                       $tblCIMC[] = $result;
+                   }
+                   break;
 
-                // Add the Parent, this lets us work out our hierarchy for display later.
-                $result['parent'] = $tblcbQosObjects['1.3.6.1.4.1.9.9.166.1.5.1.1.4'][$spid][$spobj];
-                $result['direction'] = $tblcbQosServicePolicy['1.3.6.1.4.1.9.9.166.1.1.1.1.3'][$spid];
-                $result['ifindex'] = $tblcbQosServicePolicy['1.3.6.1.4.1.9.9.166.1.1.1.1.4'][$spid];
+               // System Board - /sys/rack-unit-1/board
+               case "1.3.6.1.4.1.9.9.719.1.9.6.1":
+                   d_echo("System Board (".$tbl."): ".print_r($array[2], true)."\n");
+                   break;
 
-                // Gather different data depending on the type.
-                switch ($type) {
-                    case 1:
-                        // Policy-map, get data from that table.
-                        d_echo("\nIndex: ".$index."\n");
-                        d_echo("    UID: ".$result['UID']."\n");
-                        d_echo("    SPID.SPOBJ: ".$result['sp-id'].".".$result['sp-obj']."\n");
-                        d_echo("    If-Index: ".$result['ifindex']."\n");
-                        d_echo("    Type: 1 - Policy-Map\n");
-                        $result['label'] = $tblcbQosPolicyMapCfg['1.3.6.1.4.1.9.9.166.1.6.1.1.1'][$index];
-                        if ($tblcbQosPolicyMapCfg['1.3.6.1.4.1.9.9.166.1.6.1.1.2'][$index] != "") {
-                            $result['label'] .= " - ".$tblcbQosPolicyMapCfg['1.3.6.1.4.1.9.9.166.1.6.1.1.2'][$index];
-                        }
-                        d_echo("    Label: ".$result['label']."\n");
-                        break;
-                    case 2:
-                        // Class-map, get data from that table.
-                        d_echo("\nIndex: ".$index."\n");
-                        d_echo("    UID: ".$result['UID']."\n");
-                        d_echo("    SPID.SPOBJ: ".$result['sp-id'].".".$result['sp-obj']."\n");
-                        d_echo("    If-Index: ".$result['ifindex']."\n");
-                        d_echo("    Type: 2 - Class-Map\n");
-                        $result['label'] = $tblcbQosClassMapCfg['1.3.6.1.4.1.9.9.166.1.7.1.1.1'][$index];
-                        if($tblcbQosClassMapCfg['1.3.6.1.4.1.9.9.166.1.7.1.1.2'][$index] != "") {
-                            $result['label'] .= " - ".$tblcbQosClassMapCfg['1.3.6.1.4.1.9.9.166.1.7.1.1.2'][$index];
-                        }
-                        d_echo("    Label: ".$result['label']."\n");
-                        if ($tblcbQosClassMapCfg['1.3.6.1.4.1.9.9.166.1.7.1.1.3'][$index] == 2) {
-                            $result['map-type'] = 'Match-All';
-                        }
-                        elseif ($tblcbQosClassMapCfg['1.3.6.1.4.1.9.9.166.1.7.1.1.3'][$index] == 3) {
-                            $result['map-type'] = 'Match-Any';
-                        }
-                        else {
-                            $result['map-type'] = 'None';
-                        }
+               // Power Stats - /sys/rack-unit-1/board/power-stats
+               case "1.3.6.1.4.1.9.9.719.1.9.14.1":
+                   d_echo("Power Stats (".$tbl."): ".print_r($array, true)."\n");
+                   break;
 
-                        // Find a child, this will be a type 3
-                        foreach ($tblcbQosObjects['1.3.6.1.4.1.9.9.166.1.5.1.1.4'][$spid] as $id => $value) {
-                            if ($value == $result['sp-obj']) {
-                                // We have our child, import the match
-                                if ($tblcbQosObjects['1.3.6.1.4.1.9.9.166.1.5.1.1.3'][$spid][$id] == 3) {
-                                    $result['match'] = $result['map-type'].": ".$tblcbQosMatchStmtCfg['1.3.6.1.4.1.9.9.166.1.8.1.1.1'][$tblcbQosObjects['1.3.6.1.4.1.9.9.166.1.5.1.1.2'][$spid][$id]];
-                                    d_echo("    Match: ".$result['match']."\n");
-                                }
-                            }
-                        }
-                        break;
-                    default:
-                        continue 2;
-                }
+               // Temperature Stats - /sys/rack-unit-1/board/temp-stats
+               case "1.3.6.1.4.1.9.9.719.1.9.44.1":
+//                   d_echo("Temperature Stats (".$tbl."): ".print_r($array, true)."\n");
+                   break;
 
-                $tblCBQOS[] = $result;
-            }
+               // Memory Modules - /sys/rack-unit-1/board/memarray-1/mem-0
+               case "1.3.6.1.4.1.9.9.719.1.30.11.1":
+//                   d_echo("Memory Modules (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // Memory Stats - /sys/rack-unit-1/memarray-1/mem-1/dimm-env-stats
+               case "1.3.6.1.4.1.9.9.719.1.30.12.1":
+//                   d_echo("Memory Statistics (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // CPU's - /sys/rack-unit-1/board/cpu-1
+               case "1.3.6.1.4.1.9.9.719.1.41.9.1":
+                   d_echo("CPU's (".$tbl."): ".print_r($array[2], true)."\n");
+                   break;
+
+               // CPU Stats - /sys/rack-unit-1/board/cpu-1/env-stats
+               case "1.3.6.1.4.1.9.9.719.1.41.2.1":
+//                   d_echo("CPU Stats (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // SAS Storage Module - /sys/rack-unit-1/board/storage-SAS-2
+               case "1.3.6.1.4.1.9.9.719.1.45.1.1":
+//                   d_echo("SAS Modules (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // SAS Disks - /sys/rack-unit-1/board/storage-SAS-2/disk-1
+               case "1.3.6.1.4.1.9.9.719.1.45.4.1":
+//                   d_echo("SAS Disks (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // LUN's - /sys/rack-unit-1/board/storage-SAS-2/lun-0
+               case "1.3.6.1.4.1.9.9.719.1.45.8.1":
+  //                 d_echo("LUN's (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // RAID Battery - /sys/rack-unit-1/board/storage-SAS-2/raid-battery
+               case "1.3.6.1.4.1.9.9.719.1.45.11.1":
+//                   d_echo("RAID Battery (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // Fan's - /sys/rack-unit-1/fan-module-1-1/fan-1
+               case "1.3.6.1.4.1.9.9.719.1.15.12.1":
+//                   d_echo("System Fan's (".$tbl."): ".print_r($array, true)."\n");
+                   break;
+
+               // PSU's - /sys/rack-unit-1/psu-1
+               case "1.3.6.1.4.1.9.9.719.1.15.56.1":
+                   $result = array();
+                   d_echo("Power Supplies (".$tbl."): ".print_r($array, true)."\n");
+                   foreach ($array[3] as $key => $item) {
+                       $result['label'] = preg_replace('/\/sys|sys/','',$array[2][$key]);
+                       $result['parent'] = preg_replace('/\/'.$item.'/','',$result['label']);
+                       $result['serial'] = $array[13][$key];
+//                       $result['string'] = $array[32][$key] ." - ". ($array[49][$key]/1024) ."G Mem, ". $array[36][$key] ." CPU, ". $array[35][$key] ." core";
+
+                       // Does this entity have an entry in the faults table.
+                       if (isset ($faults[$result['label']])) {
+                           // Yes, report an error
+                           $result['status'] = 0;
+                           $result['error'] = $faults[$result['label']];
+                       }
+                       else {
+                           // No, unset any errors that may exist.
+                           $result['status'] = 1;
+                           $result['error'] = '';
+                       }
+
+                       // Add the result to the array.
+                       d_echo("Power Supplies (".$tbl."): ".print_r($result, true)."\n");
+                       $tblCIMC[] = $result;
+                   }
+                   break;
+
+               // Unknown Table, ask the user to log an issue so this can be identified.
+               default:
+//                   d_echo("Cisco-CIMC Error...\n");
+//                   d_echo("    Unknown Table: ".$tbl."\n");
+//                   d_echo("\n");
+                   break;
+           }
+
         }
 
         /*
