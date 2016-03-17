2a6832e5e1d52ecf2b770055eafa5e3d72f467a0
Aaron Daniels <aaron@daniels.id.au>
2016-03-18 06:32:16 +1000

    - HACK - if this ever makes it to a PR, please do not merge!!! - This mod counts the OU's in a search Base and graphs a count of the CN objects in each OU.  used to graph child AD objects in a radiantlogic database.

--- includes/defaults.inc.php
+++ includes/defaults.inc.php
@@ -715,6 +715,7 @@ $config['poller_modules']['cisco-voice']                 = 1;
 $config['poller_modules']['cisco-cbqos']                 = 1;
 $config['poller_modules']['stp']                         = 1;
 $config['poller_modules']['cisco-otv']                   = 1;
+$config['poller_modules']['ldap_usersperou']             = 0;
 
 // List of discovery modules. Need to be in this array to be
 // considered for execution.
@@ -750,6 +751,7 @@ $config['discovery_modules']['charge']         = 1;
 $config['discovery_modules']['cisco-cbqos']    = 0;
 $config['discovery_modules']['stp']            = 1;
 $config['discovery_modules']['cisco-otv']      = 1;
+$config['discovery_modules']['ldap_usersperou']= 0;
 
 $config['modules_compat']['rfc1628']['liebert']    = 1;
 $config['modules_compat']['rfc1628']['netmanplus'] = 1;
