28353a02d284ccc0ec39420fe323964f2a94b127
Aaron Daniels <aaron@daniels.id.au>
2016-07-13 07:28:55 +1000

    .

--- includes/polling/os/vccodec.inc.php
+++ includes/polling/os/vccodec.inc.php
@@ -3,8 +3,8 @@
 // Cisco CodecSoftW: ce8.1.0.b8c0ca3MCU: Cisco TelePresence SX20Date: 2016-04-05S/N: FTT193002F3
 d_echo("Descr: ".$device['sysDescr']);
 
-if (preg_match('/^Cisco CodecSoftW ([^,]+)MCU: Cisco Telepresence ([^,]+)Date: [^,]+S/N: ([^,]+)$/', $device['sysDescr'], $regexp_result)) {
-d_echo("MATCH! - ".print_r($regexp_result));
+//if (preg_match('/^Cisco CodecSoftW ([^,]+)MCU: Cisco Telepresence ([^,]+)Date: [^,]+S/N: ([^,]+)$/', $device['sysDescr'], $regexp_result)) {
+if (preg_match('/Cisco CodecSoftW ([^,]+)/', $device['sysDescr'], $regexp_result)) {
     $version  = $regexp_result[1];
     $hardware = $regexp_result[2];
     $serial = $regexp_result[3];
