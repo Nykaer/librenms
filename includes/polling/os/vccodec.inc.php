4c23623dcb7bfa54ea6444c5ad43a8370e818874
Aaron Daniels <aaron@daniels.id.au>
2016-08-15 07:31:38 +1000

    - .

--- includes/polling/os/vccodec.inc.php
+++ includes/polling/os/vccodec.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-// Cisco CodecSoftW: ce8.1.0.b8c0ca3MCU: Cisco TelePresence SX20Date: 2016-04-05S/N: FTT193002F3
+// Cisco CodecSoftW: ce8.1.0.b8c0ca3MCU: Cisco TelePresence SX20Date: 2016-04-05S/N: 0101010101
 if (preg_match('/^Cisco CodecSoftW: ([^,]+)MCU: Cisco TelePresence ([^,]+)Date: [^,]+S\/N: ([^,]+)$/', $device['sysDescr'], $regexp_result)) {
     $version  = $regexp_result[1];
     $hardware = $regexp_result[2];
