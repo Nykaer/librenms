e30952f8d16d2db2309539e8eb07a1ad562a41aa
Aaron Daniels <aaron@daniels.id.au>
2016-08-16 17:12:30 +1000

    - Fixed merge errors

--- includes/definitions.inc.php
+++ includes/definitions.inc.php
@@ -574,8 +574,6 @@ $config['os'][$os]['over'][4]['graph'] = 'device_ciscowlc_numclients';
 $config['os'][$os]['over'][4]['text']  = 'Number of Clients';
 $config['os'][$os]['icon']             = 'cisco';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 // CUOS - Unified Communications OS
 $os = "ucos";
 $config['os'][$os]['text']              = "Unified Communications OS";
